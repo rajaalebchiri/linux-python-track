9793809fb134404ca7bc745f575a7cde7203bd0c
rajaa lebchiri <rajaa.lebchiri@gmail.com>
2025-05-25 23:21:19 +0100

    useful decorators

---
+++ decorators/script.py
@@ -0,0 +1,90 @@
+import time
+
+def myDecorator(function):
+
+    def wrapper(*args, **kwargs):
+        value = function(*args, **kwargs)
+        print("i am in the middle")
+        return value
+
+    return wrapper
+
+@myDecorator
+def hello_world(person):
+    return "Hello, World!", person
+
+hey = hello_world("mike")
+
+## Example #1 Logging Decorator
+
+def logging(function):
+    def wrapper(*args, **kwargs):
+        value = function(*args, **kwargs)
+        with open("logfile.txt", "a+") as f:
+            fname = function.__name__
+            print(f"{fname} returned {value}")
+            f.write(f"{fname} returned {value}\n")
+        return value
+    return wrapper
+
+@logging
+def add(x, y):
+    return x + y
+
+## Example #2 Timing
+
+def timing(function):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        value = function(*args, **kwargs)
+        end = time.time()
+        fname = function.__name__
+        print(f"{fname} took {end - start} seconds to run")
+        return value
+    return wrapper
+
+@timing
+def check_numbers(x, y):
+    for i in range(x - y, 1, 10):
+        print(i)
+
+
+## Useful Examples
+# Property, to control the access to an attribute
+class Circle:
+    def __init__(self, radius):
+        self._radius = radius
+    
+    @property
+    def radius(self):
+        return self._radius
+    
+    @radius.setter
+    def radius(self, value):
+        if value >= 0:
+            self._radius = value
+        else:
+            raise ValueError("Radius cannot be negative")
+        
+    @property
+    def diameter(self):
+        return self._radius * 2
+    
+    @radius.deleter
+    def radius(self):
+        del self._radius
+
+
+# STATICMETHOD
+class Math:
+    @staticmethod
+    def add(x, y):
+        return x + y
+    
+    @staticmethod
+    def multiply(x, y):
+        return x * y
+
+
+print(Math.add(5,8))
+print(Math.multiply(5,8))
