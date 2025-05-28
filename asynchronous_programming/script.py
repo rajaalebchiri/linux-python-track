31f1b9fb297975169344635500bf79623c714a13
rajaa lebchiri <rajaa.lebchiri@gmail.com>
2025-05-28 16:29:05 +0100

    asyncio queue

--- asynchronous_programming/script.py
+++ asynchronous_programming/script.py
@@ -1,29 +1,67 @@
 import asyncio
+from asyncio import Queue
+from random import randrange
 
-async def order_coffee(customer_name, order):
-    print(f"{customer_name} orders coffee")
-    print(f"barista prepping {order} for {customer_name}")
-    if order == "cappuccino":
-        await asyncio.sleep(4)
-    elif order == "latte":
-        await asyncio.sleep(3)
-    elif order == "espresso":
-        await asyncio.sleep(3)
-    else:
-        raise asyncio.CancelledError("Order Cancelled")
-    print(f"Coffee Ready for {customer_name}")
+class Order:
+    def __init__(self, order_name: str, prepping_time: int):
+        self.order_name = order_name
+        self.prep_time = prepping_time
 
-async def prepping():
-    queue = asyncio.Queue()
-    customers = ["alice", "Bob", "Charlie", "David"]
-    orders = ["latte", "cappuccino", "espresso", "double espresso"]
-    tasks = [
-        asyncio.create_task(order_coffee(c, o))
-        for c, o in zip(customers, orders)
+class Customer:
+    def __init__(self, id: int, customer_name: str, order: Order):
+        self.id = id
+        self.customer_name = customer_name
+        self.order = order
+
+async def place_order(queue: Queue, id: int, name: str, order_name: str, prepping_time: int):
+    order = Order(order_name= order_name, prepping_time=prepping_time)
+    customer = Customer(id=id, customer_name=name, order=order)
+    await queue.put(customer)
+
+async def prepping_order(queue: Queue, i: int):
+    while not queue.empty():
+        customer: Customer = await queue.get()
+        print(f"The barista number {i} prepping the order {customer.order.order_name} for customer {customer.customer_name}")
+        await asyncio.sleep(customer.order.prep_time)
+        queue.task_done()
+        print("The order is ready")
+
+
+async def main():
+    queue = Queue()
+    customers = [
+        {
+            "id": 1, 
+            "customer_name": "David", 
+            "order_name": "espresso", 
+            "prepping_time": .3
+        },
+        {
+            "id": 2, 
+            "customer_name": "Jack",
+            "order_name": "latte", 
+            "prepping_time": .5
+        },
+        {
+            "id": 3, 
+            "customer_name": "SOMETHING",
+            "order_name": "double esporesso",
+            "prepping_time": .7
+        }    
     ]
-    try:
-        await asyncio.wait_for(asyncio.gather(*tasks), timeout=3.5)
-    except asyncio.TimeoutError:
-        print('timeout!')
+    
 
-asyncio.run(prepping())
+    orders = [place_order(queue=queue, id=customer["id"], name=customer["customer_name"], order_name=customer["order_name"], prepping_time=customer["prepping_time"]) for customer in customers]
+    await asyncio.gather(*orders)
+
+
+    baristas = [asyncio.create_task(prepping_order(queue=queue, i=i)) for i in range(3)]
+
+
+    await queue.join()
+
+    for b in baristas:
+        b.cancel()
+
+if __name__ == "__main__":
+    asyncio.run(main())
