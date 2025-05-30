aaf8407db72adcda715730fe4eaa96d5b11f7410
rajaa lebchiri <rajaa.lebchiri@gmail.com>
2025-05-30 15:07:21 +0100

    fixing the solution

--- async-crypto-tracker-cli/script.py
+++ async-crypto-tracker-cli/script.py
@@ -13,7 +13,7 @@ parameters = {
 }
 headers = {
   'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': 'api-key',
+  'X-CMC_PRO_API_KEY': 'e25e7a84-828c-4f50-8488-859c5eed8d9e',
 }
 
 @dataclass
@@ -27,114 +27,109 @@ class Coin:
     def __str__(self):
         return f"{self.name} ({self.symbol}): ${self.price}"
 
+async def fetch_json(session, url, params=None):
+    async with session.get(url, params=params) as response:
+        response.raise_for_status()
+        return await response.json()
+
 
 async def get_coin(coin: str, n: int = None):
-
     async with aiohttp.ClientSession(headers=headers) as session:
         if n is not None:
             print(f"Getting {coin} price")
             while True:
                 print(f"Getting {coin} price at {time.strftime('%H:%M:%S')}")
-                async with session.get(f"https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest", params={"symbol": coin}) as response:
-                    data = await response.text()
-                    data = json.loads(data)
-                    coin_data = data["data"][coin]
-                    c = Coin(coin, coin_data['name'], coin_data['symbol'], coin_data['slug'], coin_data['quote']['USD']['price'])
-                    print(c)
-                await asyncio.sleep(n)
-
-        else:
-            async with session.get(f"https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest", params={"symbol": coin}) as response:
-                data = await response.text()
-                data = json.loads(data)
+                response = fetch_json(session, url="https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest", params={"symbol": coin})
+                data = await response
                 coin_data = data["data"][coin]
                 c = Coin(coin, coin_data['name'], coin_data['symbol'], coin_data['slug'], coin_data['quote']['USD']['price'])
                 print(c)
+                await asyncio.sleep(n)
+        else:
+            response = fetch_json(session, url="https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest", params={"symbol": coin})
+            data = await response
+            coin_data = data["data"][coin]
+            c = Coin(coin, coin_data['name'], coin_data['symbol'], coin_data['slug'], coin_data['quote']['USD']['price'])
+            print(c)
 
 
 async def convert_coin(amount: float, from_coin: str, to: str):
 
     async with aiohttp.ClientSession(headers=headers) as session:
-        async with session.get(f"https://pro-api.coinmarketcap.com/v1/tools/price-conversion", 
-            params={"symbol": from_coin, "convert": to, "amount": amount}) as response:
-            data = await response.text()
-            data = json.loads(data)
-            coin_data = data["data"]
-            print(f"{amount} {from_coin} is equal to {coin_data['quote'][to]['price']} {to}")
+        response = fetch_json(session, url="https://pro-api.coinmarketcap.com/v1/tools/price-conversion", params={"symbol": from_coin, "convert": to, "amount": amount})
+        data = await response
+        coin_data = data["data"]
+        print(f"{amount} {from_coin} is equal to {coin_data['quote'][to]['price']} {to}")
 
             
     
-
 async def get_coins(limit: int = 5):
     url = f'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit={limit}'
     async with aiohttp.ClientSession(headers=headers) as session:
-        async with session.get(url) as response:
+        response = fetch_json(session, url)
+        data = await response
 
-            data = await response.text()
-            data = json.loads(data)
+        for coin in data["data"]:
+            c = Coin(coin['id'], coin['name'], coin['symbol'], coin['slug'], coin['quote']['USD']['price'])
+            print(c)
 
-            for coin in data["data"]:
-                c = Coin(coin['id'], coin['name'], coin['symbol'], coin['slug'], coin['quote']['USD']['price'])
-                print(c)
-
-async def top_coins(limit: int = 5):
+async def top_coins(limit: int):
     url = f'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?sort=market_cap&limit={limit}'
     async with aiohttp.ClientSession(headers=headers) as session:
-        async with session.get(url) as response:
+        response = fetch_json(session, url)
+        data = await response
 
-            data = await response.text()
-            data = json.loads(data)
+        for coin in data["data"]:
+            c = Coin(coin['id'], coin['name'], coin['symbol'], coin['slug'], coin['quote']['USD']['price'])
+            print(c)
 
-            for coin in data["data"]:
-                c = Coin(coin['id'], coin['name'], coin['symbol'], coin['slug'], coin['quote']['USD']['price'])
-                print(c)
+def run_get_coin(args):
+    asyncio.run(get_coin(args.coin, args.number))
 
+def run_get_coins(_):
+    asyncio.run(get_coins())
 
+def run_top_coins(args):
+    if args.limit is None:
+        limit = 5
+    else:
+        limit = args.limit
+    asyncio.run(top_coins(limit))
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description="Crypto Tracker CLI",
+        prog="crypto-tracker-cli",
+    )
 
-    parser.add_argument("--track", help="Get current price of specific coin", action="store_true")
+    subparsers = parser.add_subparsers(title="commands", dest="command")
 
-    parser.add_argument("-c", "--coin", help="Coin to track")
+    # get current price of specific coin command
+    track_coin_parser = subparsers.add_parser("track-coin", help="Track coin price")
+    track_coin_parser.add_argument('-c', '--coin', help='Coin symbol', required=True)
+    track_coin_parser.add_argument('-n', '--number', help='Number of seconds to wait between each update', required=False, type=int)
+    track_coin_parser.set_defaults(func=run_get_coin)
 
-    parser.add_argument("-n", "--number", help="Number of seconds to wait between each update", type=int)
+    # List Top coins
+    track_coin_parser = subparsers.add_parser("top-coins", help="get top coins")
+    track_coin_parser.add_argument('-l', '--limit', help='Limit the number of coins to list', type=int)
+    track_coin_parser.set_defaults(func=run_top_coins)
 
-    parser.add_argument("--top", help="List Top coins", action="store_true")
+    # Convert Coin
+    convert_coin_parser = subparsers.add_parser("convert-coin", help="Convert Coin")
+    convert_coin_parser.add_argument('-a', '--amount', help='Amount to convert', required=True, type=float)
+    convert_coin_parser.add_argument('-f', '--from-coin', help='From Coin', required=True, type=str)
+    convert_coin_parser.add_argument('-t', '--to-coin', help='To Coin', required=True, type=str)
+    convert_coin_parser.set_defaults(func=lambda args: asyncio.run(convert_coin(args.amount, args.from_coin, args.to_coin)))
 
-    parser.add_argument("-l", "--limit", help="Limit the number of coins to list", type=int)
 
-    parser.add_argument("--convert", help="Convert Coin", action="store_true")
-
-    parser.add_argument("-a", "--amount", help="The amount you want to convert", type=int)
-
-    parser.add_argument("-f", "--from-coin", help="From Coin", type=str)
-
-    parser.add_argument("-t", "--to-coin", help="To Coin", type=str)
 
     args = parser.parse_args()
-
-
-    if args.track:
-        if not args.coin:
-            print("you need to specify the coin symbol")
-        else:
-            asyncio.run(get_coin(args.coin, args.number))
-    if args.top:
-        if args.limit:
-            asyncio.run(top_coins(args.limit))
-        else:
-            asyncio.run(top_coins())
-
-    if args.convert:
-        if args.amount and args.from_coin and args.to_coin:
-            print(f"Converting {args.amount} {args.from_coin} to {args.to_coin}")
-            asyncio.run(convert_coin(float(args.amount), args.from_coin, args.to_coin))
-        else:
-            print("you need to specify the amount, from coin, and to coin")
-        
+    if hasattr(args, "func"):
+        args.func(args)
     else:
-        asyncio.run(get_coins())
+        run_get_coins(None)
 
+                
 if __name__ == "__main__":
     main()
