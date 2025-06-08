615ed9d76ab1078e99e47c47a5c794202465210b
rajaa lebchiri <rajaa.lebchiri@gmail.com>
2025-06-08 19:35:24 +0100

    docker network multi env container

--- docker-learning/search-personal-notes/app/es_client.py
+++ docker-learning/search-personal-notes/app/es_client.py
@@ -1,18 +1,21 @@
+import os
 from elasticsearch import Elasticsearch
 
-es = Elasticsearch("http://0.0.0.0:9200")
+es_url = os.environ.get('ELASTICSEARCH_URL', 'http://es:9200')
+es = Elasticsearch(es_url)
+
 
 INDEX_NAME = "notes"
 
 def create_index_if_not_exists():
     if not es.indices.exists(index=INDEX_NAME):
-        es.indices.create(index=INDEX_NAME, body={
-            "mappings": {
-                "properties": {
-                    "title": {"type": "text"},
-                    "body": {"type": "text"},
-                    "tags": {"type": "keyword"},
-                    "created_at": {"type": "date"}
+            es.indices.create(index=INDEX_NAME, body={
+                "mappings": {
+                    "properties": {
+                        "title": {"type": "text"},
+                        "body": {"type": "text"},
+                        "tags": {"type": "keyword"},
+                        "created_at": {"type": "date"}
+                    }
                 }
-            }
-        })
+            })
